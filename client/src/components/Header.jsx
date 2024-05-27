92f0f40ea2e4867019bd3bb23aad8f7d7dc34d91
Brandon Montero <brandonmontero@Brandons-MacBook-Pro.local>
2024-05-27 13:21:09 -0600

    agregando libreria de icons, haciendo responsive del header, menu de hamburguesa y creacion de componente hero

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,58 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className="bg-[#89BAC7] flex justify-between items-center p-4">
+    <header className="bg-[#89BAC7] p-4 flex justify-between items-center">
       <div className="text-white text-2xl font-bold">MEDIBOT</div>
-      <nav className="flex gap-4">
-        <a href="#" className="text-white text-lg hover:text-[#568281]">
+      <nav
+        className={`${
+          isOpen ? "block" : "hidden"
+        } absolute top-16  right-2 w-60  bg-[#89BAC7] p-4 md:relative md:flex md:p-0 md:bg-transparent md:w-auto md:top-0 md:left-0`}
+      >
+        <a
+          href="#"
+          className="block text-white text-lg py-2 hover:text-[#568281] md:py-0 md:px-4"
+        >
           Inicio
         </a>
-        <a href="#" className="text-white text-lg hover:text-[#568281]">
+        <a
+          href="#"
+          className="block text-white text-lg py-2 hover:text-[#568281] md:py-0 md:px-4"
+        >
           Quienes Somos
         </a>
-        <a href="#" className="text-white text-lg hover:text-[#568281]">
+        <a
+          href="#"
+          className="block text-white text-lg py-2 hover:text-[#568281] md:py-0 md:px-4"
+        >
           Cómo Funciona
         </a>
-        <a href="#" className="text-white text-lg hover:text-[#568281]">
+        <a
+          href="#"
+          className="block text-white text-lg py-2 hover:text-[#568281] md:py-0 md:px-4"
+        >
           Contacto
         </a>
+        <button className="bg-white text-black py-2 px-4 flex items-center rounded-full hover:bg-[#568281] hover:text-white mt-4 md:hidden">
+          Iniciar Sesión <span className="ml-2">→</span>
+        </button>
       </nav>
-      <button className="bg-white text-black py-2 px-4 flex items-center rounded-full hover:bg-[#568281] hover:text-white">
+      <button className="bg-white text-black py-2 px-4 flex items-center rounded-full hover:bg-[#568281] hover:text-white hidden md:flex">
         Iniciar Sesión <span className="ml-2">→</span>
       </button>
+      <div className="md:hidden">
+        <button onClick={toggleMenu} className="text-white text-2xl">
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+        </button>
+      </div>
     </header>
   );
 };
