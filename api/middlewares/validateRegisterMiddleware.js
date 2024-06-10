4e60c5652eec893f792e8c5275b3fbb290d9b3ab
vale1b <barberisvalentin85@gmail.com>
2024-06-10 17:35:40 -0300

    Adaptando validaciones de registro al formulario.

--- api/middlewares/validateRegisterMiddleware.js
+++ api/middlewares/validateRegisterMiddleware.js
@@ -18,30 +18,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 const validateRegisterMiddleware = [
-  body("name").notEmpty().withMessage("El nombre es obligatorio"),
-  body("lastName").notEmpty().withMessage("El apellido es obligatorio"),
-  body("phone").notEmpty().withMessage("El teléfono es obligatorio"),
   body("email").notEmpty().withMessage("El email es obligatorio")
     .bail().isEmail().withMessage("El email no es válido"),
-  body("birthdate").notEmpty().withMessage("La fecha de nacimiento es obligatoria")
-    .bail().isISO8601().withMessage("La fecha de nacimiento debe ser una fecha válida"),
-  body("address").notEmpty().withMessage("El domicilio es obligatorio"),
   body("password").notEmpty().withMessage("La contraseña es obligatoria")
     .bail().isLength({ min: 8 }).withMessage("La contraseña debe tener al menos 8 caracteres")
     .matches(/^(?=.*[A-Z])/).withMessage("La contraseña debe tener al menos una mayúscula"),
-  body("imageProfile").custom((value, { req }) => {
-    let file = req.file;
-    let acceptedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
-    if (!file) {
-      throw new Error("Tienes que subir una imagen");
-    } else {
-      let fileExtension = path.extname(file.originalname);
-      if (!acceptedExtensions.includes(fileExtension)) {
-        throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(", ")}`);
-      }
-    }
-    return true;
-  }),
 ];
 
 app.use(express.json());
